6781f16937fc814c7802f4c9143e0ea5fdf64591
Wilson da Silva Gorosthides Neto <netogoro77@gmail.com>
2026-09-03 12:13:22 -0400

    chore: esqueleto Spring Boot 4.1.1 com wrapper do Maven

---
+++ src/main/java/br/com/wilson/criptoapi/CriptoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.wilson.criptoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wilson/criptoapi/CriptoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wilson.criptoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
